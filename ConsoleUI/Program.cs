c5910ea52bd83cfc90c1acf692433cfeb661456d
Alper Çelik <alper.celikk1@outlook.com>
2021-02-03 16:28:36 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+
+                
+            }
+            Console.WriteLine("------------------- BrandId'ye göre Sonuç. -------------------");
+            foreach (var car in carManager.GetById(3))
+            {
+                Console.WriteLine("BrandId'si "+car.BrandId+" olan araçlar: "+ car.Description);
+            }
+            
+          
+        }
+    }
+}
